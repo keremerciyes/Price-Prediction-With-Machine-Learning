f6d99ee8acd6e5f7714766f829e4cb2c0fd0e2cc
oktaykurt <oktaykurt2000@gmail.com>
2022-11-02 16:47:10 +0300

    Arma Sunspots Updated

--- arma_sunspots.py
+++ arma_sunspots.py
@@ -14,38 +14,31 @@ dta.index = pd.Index(sm.tsa.datetools.dates_from_range("1700", "2008"))
 dta.index.freq = dta.index.inferred_freq
 del dta["YEAR"]
 
-dta.plot(figsize=(12, 8))
 
 
-fig = plt.figure(figsize=(12, 8))
-ax1 = fig.add_subplot(211)
-fig = sm.graphics.tsa.plot_acf(dta.values.squeeze(), lags=40, ax=ax1)
-ax2 = fig.add_subplot(212)
-fig = sm.graphics.tsa.plot_pacf(dta, lags=40, ax=ax2)
-
-arma_mod20 = ARIMA(dta, order=(3, 0, 0)).fit()
+arma_mod20 = ARIMA(dta, order=(1, 0, 1)).fit()
 print(arma_mod20.params)
 sm.stats.durbin_watson(arma_mod20.resid.values)
 
-fig = plt.figure(figsize=(12, 8))
-ax = fig.add_subplot(111)
-ax = arma_mod20.resid.plot(ax=ax)
+# fig = plt.figure(figsize=(12, 8))
+# ax = fig.add_subplot(111)
+# ax = arma_mod20.resid.plot(ax=ax)
 
 
 resid = arma_mod20.resid
 
 stats.normaltest(resid)
 
-fig = plt.figure(figsize=(12, 8))
-ax = fig.add_subplot(111)
-fig = qqplot(resid, line="q", ax=ax, fit=True)
+# fig = plt.figure(figsize=(12, 8))
+# ax = fig.add_subplot(111)
+# fig = qqplot(resid, line="q", ax=ax, fit=True)
 
 
-fig = plt.figure(figsize=(12, 8))
-ax1 = fig.add_subplot(211)
-fig = sm.graphics.tsa.plot_acf(resid.values.squeeze(), lags=40, ax=ax1)
-ax2 = fig.add_subplot(212)
-fig = sm.graphics.tsa.plot_pacf(resid, lags=40, ax=ax2)
+# fig = plt.figure(figsize=(12, 8))
+# ax1 = fig.add_subplot(211)
+# fig = sm.graphics.tsa.plot_acf(resid.values.squeeze(), lags=40, ax=ax1)
+# ax2 = fig.add_subplot(212)
+# fig = sm.graphics.tsa.plot_pacf(resid, lags=40, ax=ax2)
 
 r, q, p = sm.tsa.acf(resid.values.squeeze(), fft=True, qstat=True)
 data = np.c_[np.arange(1, 25), r[1:], q, p]
@@ -54,11 +47,16 @@ table = pd.DataFrame(data, columns=["lag", "AC", "Q", "Prob(>Q)"])
 print(table.set_index("lag"))
 
 
-predict_sunspots = arma_mod20.predict("1920", "1970", dynamic=True)
+predict_sunspots = arma_mod20.predict("1970", "2050", dynamic=True)
 print(predict_sunspots)
+
 fig = plt.figure(figsize=(12, 8))
+
 ax3 = fig.add_subplot(111)
-plt.plot(predict_sunspots)
+predict_sunspots.plot(ax = ax3, color='b')
+dta.plot(ax = ax3)
+
+
 
 def mean_forecast_err(y, yhat):
     return y.sub(yhat).mean()
